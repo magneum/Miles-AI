3f300e799aba748110f283c0fa5b81673fad62bf
magneum <krakinzkon@gmail.com>
2023-03-31 01:08:22 +0530

    Miles Ai python voice assistant

--- reviews/model.py
+++ reviews/model.py
@@ -1,7 +1,62 @@
+import torch
 import torch.nn as nn
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+
+# Define a function to load the pre-trained word embeddings
+def load_embeddings(embeddings_file):
+    # Load the pre-trained word embeddings from the file
+    with open(embeddings_file, "r", encoding="utf-8") as f:
+        word_embeddings = {}
+        for line in f:
+            values = line.split()
+            word = values[0]
+            vector = np.asarray(values[1:], dtype="float32")
+            word_embeddings[word] = vector
+    return word_embeddings
 
 
-# Define a class called NeuralNet that inherits from the nn.Module class
+# Define a class to represent the dataset
+class TextClassificationDataset(Dataset):
+    def __init__(self, data, word_embeddings, max_sequence_length):
+        self.data = data
+        self.word_embeddings = word_embeddings
+        self.max_sequence_length = max_sequence_length
+        self.pad_token = np.zeros_like(next(iter(word_embeddings.values())))
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        # Get the text and label at the specified index
+        text, label = self.data[idx]
+        # Convert the text to a sequence of word embeddings
+        sequence = []
+        for word in text.split():
+            if word in self.word_embeddings:
+                sequence.append(self.word_embeddings[word])
+            else:
+                sequence.append(np.random.normal(scale=0.1, size=self.pad_token.shape))
+        # Pad or truncate the sequence to the desired length
+        if len(sequence) < self.max_sequence_length:
+            sequence = np.concatenate(
+                (
+                    sequence,
+                    np.tile(
+                        self.pad_token, (self.max_sequence_length - len(sequence), 1)
+                    ),
+                )
+            )
+        else:
+            sequence = sequence[: self.max_sequence_length]
+        # Convert the sequence to a PyTorch tensor
+        sequence = torch.from_numpy(np.array(sequence))
+        # Convert the label to a PyTorch tensor
+        label = torch.tensor(label)
+        return sequence, label
+
+
+# Define a class representing a neural network with three linear layers.
 class NeuralNet(nn.Module):
     """
     A class representing a neural network with three linear layers.
