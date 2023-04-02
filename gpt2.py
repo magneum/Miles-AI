14723946da9993d527c8aac5984070f472cf9847
Mågnëum / <krakinzkon@gmail.com>
2023-04-02 19:23:48 +0530

    Miles Ai Voice Assistant

--- gpt2.py
+++ gpt2.py
@@ -1,36 +1,36 @@
+# Import necessary libraries
 import pandas as pd
 from tensorflow import keras
 from tensorflow.keras import layers
 from keras_tuner import RandomSearch
 from spellchecker import SpellChecker
 
+# Initialize the spellchecker
 spell = SpellChecker()
 
+# Load the training, validation, and test datasets
 train_data = pd.read_csv("corpdata/gpt/large-762M-k40.train.csv")
 print("Train data shape:", train_data.shape)
-print("Train data head:")
-print(train_data.head())
+print("Train data head:", train_data.head())
 
 valid_data = pd.read_csv("corpdata/gpt/large-762M-k40.valid.csv")
 print("Validation data shape:", valid_data.shape)
-print("Validation data head:")
-print(valid_data.head())
+print("Validation data head:", valid_data.head())
 
 test_data = pd.read_csv("corpdata/gpt/large-762M-k40.test.csv")
 print("Test data shape:", test_data.shape)
-print("Test data head:")
-print(test_data.head())
+print("Test data head:", test_data.head())
 
+# Split the datasets into inputs and outputs
 train_inputs = train_data["input"].values
 train_outputs = train_data["output"].values
-
 valid_inputs = valid_data["input"].values
 valid_outputs = valid_data["output"].values
-
 test_inputs = test_data["input"].values
 test_outputs = test_data["output"].values
 
 
+# Define a function to build the model using hyperparameters
 def build_model(hp):
     model = keras.Sequential()
     model.add(
@@ -53,25 +53,27 @@ def build_model(hp):
     return model
 
 
+# Define a RandomSearch object to search for the best hyperparameters
 tuner = RandomSearch(
     build_model, objective="val_mae", max_trials=5, executions_per_trial=3
 )
+
+# Search for the best hyperparameters
 tuner.search(
     train_inputs, train_outputs, epochs=5, validation_data=(valid_inputs, valid_outputs)
 )
 
+# Get the best model and evaluate it on the test dataset
 best_model = tuner.get_best_models(num_models=1)[0]
 test_loss, test_mae = best_model.evaluate(test_inputs, test_outputs)
 
+# Print the best model summary and hyperparameters
 print("Best Model Summary:")
 print(best_model.summary())
-
 print("Best Hyperparameters:")
 print(tuner.get_best_hyperparameters(num_trials=1)[0].values)
 
-print("Test Mean Absolute Error:", test_mae)
-
-
+# Define a function to generate a response using the best model
 def generate_response(input_str):
     words = input_str.split()
     misspelled_words = spell.unknown(words)
@@ -84,12 +86,14 @@ def generate_response(input_str):
     return prediction
 
 
+# Start the chatbot loop
 print("Hello! I am a chatbot. How can I help you today?")
 while True:
     user_input = input("> ")
     if user_input.lower() == "quit":
         print("Goodbye!")
         break
+    # Check for misspelled words and correct them if found
     words = user_input.split()
     misspelled_words = spell.unknown(words)
     if misspelled_words:
@@ -98,5 +102,6 @@ while True:
             corrected_word = spell.correction(word)
             user_input = user_input.replace(word, corrected_word)
         print("Corrected input:", user_input)
+    # Generate a response using the best model
     response = generate_response(user_input)
     print("Model response:", response)
