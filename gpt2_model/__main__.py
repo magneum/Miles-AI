11c2eb68c888925790a312a4990969ff736a71c9
Mågnëum / <krakinzkon@gmail.com>
2023-04-02 22:45:59 +0530

    Miles Ai Voice Assistant

--- gpt2_model/__main__.py
+++ gpt2_model/__main__.py
@@ -1,8 +1,13 @@
 import pandas as pd
+from tensorflow import keras
+from tensorflow import keras
 from colorama import Fore as Fr
 from colorama import Style as Sr
+from keras_tuner import Hyperband
+from tensorflow.keras.optimizers import Adam
 from keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Dense, Dropout, LSTM, Bidirectional
 
 
 for dataset in [
@@ -50,3 +55,80 @@ for dataset in [
     print(f"{Fr.BLUE}{dataset} TRAIN OUTPUTS SHAPE:{Sr.RESET_ALL}", train_outputs.shape)
     print(f"{Fr.BLUE}{dataset} VALID OUTPUTS SHAPE:{Sr.RESET_ALL}", valid_outputs.shape)
     print(f"{Fr.BLUE}{dataset} TEST OUTPUTS SHAPE:{Sr.RESET_ALL}", test_outputs.shape)
+
+
+def build_model(hp):
+    model = keras.Sequential()
+
+    # Tune the number of LSTM layers
+    for i in range(hp.Int("num_lstm_layers", 1, 3)):
+        model.add(
+            Bidirectional(
+                LSTM(
+                    units=hp.Int(
+                        f"lstm_{i}_units", min_value=32, max_value=512, step=32
+                    )
+                )
+            )
+        )
+        # Add dropout regularization to the LSTM layers
+        model.add(
+            Dropout(
+                rate=hp.Float(
+                    f"lstm_{i}_dropout", min_value=0.1, max_value=0.5, step=0.1
+                )
+            )
+        )
+
+    # Add a dense layer to reduce the dimensionality before the output layer
+    model.add(
+        Dense(
+            units=hp.Int("dense_units", min_value=32, max_value=512, step=32),
+            activation=hp.Choice("dense_activation", values=["relu", "tanh"]),
+        )
+    )
+
+    # Add dropout regularization to the dense layer
+    model.add(
+        Dropout(rate=hp.Float("dense_dropout", min_value=0.1, max_value=0.5, step=0.1))
+    )
+
+    # Add the output layer
+    model.add(Dense(units=1, activation="sigmoid"))
+
+    # Compile the model with the specified optimizer and learning rate
+    model.compile(
+        optimizer=Adam(
+            learning_rate=hp.Choice("learning_rate", values=[1e-2, 1e-3, 1e-4])
+        ),
+        loss="binary_crossentropy",
+        metrics=["accuracy"],
+    )
+
+    return model
+
+
+tuner = Hyperband(
+    build_model,
+    objective="val_accuracy",
+    max_epochs=10,
+    factor=3,
+    directory="corpdata/gpt",
+    project_name="gpt_tuning",
+)
+
+tuner.search(
+    x=train_inputs,
+    y=train_outputs,
+    epochs=10,
+    validation_data=(valid_inputs, valid_outputs),
+)
+
+best_model = tuner.get_best_models(num_models=1)[0]
+test_loss, test_acc = best_model.evaluate(test_inputs, test_outputs, verbose=0)
+print("Test accuracy:", test_acc)
+print("Best Model Summary:")
+print(best_model.summary())
+print("Best Hyperparameters:")
+print(tuner.get_best_hyperparameters(num_trials=1)[0].values)
+best_model.save("gpt2_model.h5")
