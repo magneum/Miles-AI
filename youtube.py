7164efecd7739ef8182cd8f131b0056eb4742941
Mågnëum / <krakinzkon@gmail.com>
2023-04-04 22:08:05 +0530

    Miles Ai Voice Assistant

--- youtube.py
+++ youtube.py
@@ -1,67 +1,55 @@
+import spacy
 import requests
 
-base_url = "http://localhost:3000/"
+BASE_URL = "http://localhost:3000"
+
+ENDPOINTS = {
+    "searchVideo": "/searchVideo",
+    "getVideoDetails": "/getVideoDetails",
+    "getPlaylistDetails": "/getPlaylistDetails",
+    "searchVideosByGenre": "/searchVideosByGenre",
+    "getVideosFromChannel": "/getVideosFromChannel",
+    "getVideoDetailsByUrl": "/getVideoDetailsByUrl",
+    "getRelatedVideos": "/getRelatedVideos",
+}
+
+nlp = spacy.load("en_core_web_sm")
 
 
-def search_videos(keyword, num_videos):
-    url = base_url + "searchVideo"
-    payload = {"keyword": keyword, "numVideos": num_videos}
-    response = requests.post(url, json=payload)
-    return response.json()
+def extract_entities(query):
+    doc = nlp(query)
+    entities = {}
+    for ent in doc.ents:
+        entities[ent.label_] = ent.text
+    return entities
 
 
-# def get_video_details(video_id):
-#     url = base_url + "getVideoDetails"
-#     payload = {"videoId": video_id}
-#     response = requests.post(url, json=payload)
-#     return response.json()
+def handle_query(query):
+    entities = extract_entities(query)
+    if "TRACK_GENRE" in entities and "TRACK_COUNT" in entities:
+        url = BASE_URL + ENDPOINTS["searchVideosByGenre"]
+        params = {
+            "genre": entities["TRACK_GENRE"],
+            "numVideos": entities["TRACK_COUNT"],
+        }
+        response = requests.get(url, params=params)
+    elif "TRACK_GENRE" in entities:
+        url = BASE_URL + ENDPOINTS["searchVideosByGenre"]
+        params = {"genre": entities["TRACK_GENRE"], "numVideos": 1}
+        response = requests.get(url, params=params)
+    elif "TRACK_ID" in entities:
+        url = BASE_URL + ENDPOINTS["getVideoDetails"]
+        params = {"videoId": entities["TRACK_ID"]}
+        response = requests.get(url, params=params)
+    else:
+        response = {"message": "I'm sorry, I didn't understand your request."}
+    return response
 
 
-# def search_videos_by_genre(genre, num_videos):
-#     url = base_url + "searchVideosByGenre"
-#     payload = {"genre": genre, "numVideos": num_videos}
-#     response = requests.post(url, json=payload)
-#     return response.json()
-
-
-# def search_topic_videos(topic, num_videos):
-#     keyword = "How to " + topic
-#     return search_videos(keyword, num_videos)
-
-
-# def get_playlist_details(playlist_id, num_videos):
-#     url = base_url + "getPlaylistDetails"
-#     payload = {"playlistId": playlist_id, "numVideos": num_videos}
-#     response = requests.post(url, json=payload)
-#     return response.json()
-
-
-# def get_related_videos(video_id, num_videos):
-#     url = base_url + "getRelatedVideos"
-#     payload = {"relatedToVideoId": video_id, "numVideos": num_videos}
-#     response = requests.post(url, json=payload)
-#     return response.json()
-
-
-# def get_videos_from_channel(channel_id, num_videos):
-#     url = base_url + "getVideosFromChannel"
-#     payload = {"channelId": channel_id, "numVideos": num_videos}
-#     response = requests.post(url, json=payload)
-#     return response.json()
-
-
-# def get_video_details_by_url(video_url):
-#     url = base_url + "getVideoDetailsByUrl"
-#     payload = {"url": video_url}
-#     response = requests.post(url, json=payload)
-#     return response.json()
-
-
-print(search_videos("Taylor Swift", 10))
-# print(get_video_details("dQw4w9WgXcQ"))
-# print(search_videos_by_genre("Cooking", 10))
-# print(search_topic_videos("meditate", 5))
-# print(get_playlist_details("PLMC9KNkIncKtPzgY-5rmhvj7fax8fdxoj", 10))
-# print(get_related_videos("dQw4w9WgXcQ", 10))
-# print(get_videos_from_channel("UC-lHJZR3Gqxm24_Vd_AJ5Yw", 10))
-# print(get_video_details_by_url("https://www.youtube.com/watch?v=dQw4w9WgXcQ"))
+query = input("What would you like me to do? ")
+response = handle_query(query)
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Error: {response.status_code}")
