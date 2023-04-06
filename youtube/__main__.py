fb2b799890d0fac8d911986f94fbebd0de85540b
Mågnëum / <krakinzkon@gmail.com>
2023-04-06 12:05:36 +0530

    Miles

--- youtube/__main__.py
+++ youtube/__main__.py
@@ -189,7 +189,7 @@ print(Fore.GREEN + "Creating tuner..." + Style.RESET_ALL)
 tuner = RandomSearch(
     my_hyper_model,
     objective="val_accuracy",
-    max_trials=60,
+    max_trials=20,
     executions_per_trial=20,
     directory="youtube/model",
     project_name="youtube",
@@ -200,7 +200,7 @@ print(Fore.GREEN + "Callbacks created." + Style.RESET_ALL)
 tuner.search(
     x=train_x,
     y=train_y,
-    epochs=100,
+    epochs=50,
     batch_size=8,
     validation_data=(val_x, val_y),
     verbose=1,
