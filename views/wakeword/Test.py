4b358cdcdd2f3227c69ca5ced4c8f581fb46ab6a
Mågnëum / <krakinzkon@gmail.com>
2023-04-09 18:35:15 +0530

    Miles

--- views/wakeword/Test.py
+++ views/wakeword/Test.py
@@ -1,17 +1,19 @@
 import librosa
 import numpy as np
 import sounddevice as sd
+from colorama import Fore, Style
 from scipy.io.wavfile import write
 from tensorflow.python.keras.models import load_model
+
 
 fs = 44100
 seconds = 2
 filename = "prediction.wav"
-model = load_model("models/wakeword/best_wake_word.h5")
+model = load_model("models/wakeword/best_hyper_model.h5")
 class_names = ["Wake Word NOT Detected", "Wake Word Detected"]
 
-print("Prediction Started: ")
 i = 0
+print(f"{Fore.YELLOW}Prediction Started!")
 while True:
     print("Say Now: ")
     myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
@@ -22,9 +24,9 @@ while True:
     mfcc_processed = np.mean(mfcc.T, axis=0)
     prediction = model.predict(np.expand_dims(mfcc_processed, axis=0))
     if prediction[:, 1] > 0.99:
-        print(f"Wake Word Detected for ({i})")
+        print(f"{Fore.GREEN}Wake Word Detected for ({i}){Style.RESET_ALL}")
         print("Confidence:", prediction[:, 1])
         i += 1
     else:
-        print(f"Wake Word NOT Detected")
+        print(f"{Fore.RED}Wake Word NOT Detected{Style.RESET_ALL}")
         print("Confidence:", prediction[:, 0])
