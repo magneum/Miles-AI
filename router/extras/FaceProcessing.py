7a0753c3a0aebfafeaee8ff8694cf024867d1d2e
Mågnëum / <krakinzkon@gmail.com>
2023-04-12 21:03:40 +0530

    Miles Voice Assistant

--- router/extras/FaceProcessing.py
+++ router/extras/FaceProcessing.py
@@ -1,11 +1,44 @@
 import os
+import random
 import numpy
 import keras
 import pandas
 from colorama import Fore, Style
 from keras.callbacks import EarlyStopping
 from keras_tuner.tuners import RandomSearch
-from router.exports.CodeSeparator import CodeSeparator
+
+# from router.exports.CodeSeparator import CodeSeparator
+
+
+def CodeSeparator(section_name):
+    separator_width = 50
+    separator_char = "*"
+    section_label = f" {section_name} "
+    section_label_width = separator_width - 2
+    section_label_padding = (section_label_width - len(section_label)) // 2
+    separator_line = separator_char * separator_width
+    available_colors = [
+        Fore.RED,
+        Fore.GREEN,
+        Fore.YELLOW,
+        Fore.BLUE,
+        Fore.MAGENTA,
+        Fore.CYAN,
+        Fore.WHITE,
+    ]
+    random_color = random.choice(available_colors)
+    section_label_line = (
+        separator_char
+        + " " * section_label_padding
+        + f"{random_color}{Style.BRIGHT}{section_name}{Style.RESET_ALL}"
+        + " " * section_label_padding
+        + separator_char
+    )
+
+    print(separator_line)
+    print(section_label_line)
+    print(separator_line)
+
 
 # ============================================================ [ CREATED BY MAGNEUM ] ============================================================
 print(f"{Fore.YELLOW}{Style.BRIGHT}Code Description: FaceProcessing.py")
