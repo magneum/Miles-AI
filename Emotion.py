14471c560d98b57eaa1e3e1106c1d72198dedd21
Mågnëum / <krakinzkon@gmail.com>
2023-04-12 11:10:01 +0530

    Miles

--- Emotion.py
+++ Emotion.py
@@ -6,7 +6,7 @@ from keras_tuner.tuners import RandomSearch, Hyperband
 
 X = []
 y = []
-num_epocs = 1000
+num_epocs = 100
 num_val_split = 0.2
 fer2013 = pd.read_csv("corpdata/csv/fer2013.csv")
 for index, row in fer2013.iterrows():
@@ -29,20 +29,23 @@ def build_model(hp):
             input_shape=(48, 48, 1),
         )
     )
-    model.add(layers.MaxPooling2D(pool_size=hp.Choice("pool_size_1", values=[2, 3])))
+    model.add(layers.MaxPooling2D(pool_size=hp.Choice("pool_size_1", values=[4, 5])))
     for i in range(hp.Int("num_blocks", 1, 4)):
         model.add(
             layers.Conv2D(
                 filters=hp.Int("filters_" + str(i + 2), 32, 128, step=32),
                 kernel_size=hp.Choice("kernel_size_" + str(i + 2), values=[3, 5]),
                 activation="relu",
+                padding="same",  # Add padding to prevent downsampling
             )
         )
         model.add(
             layers.MaxPooling2D(
-                pool_size=hp.Choice("pool_size_" + str(i + 2), values=[2, 3])
+                pool_size=hp.Choice("pool_size_" + str(i + 2), values=[4, 5]),
+                padding="same",  # Add padding to prevent downsampling
             )
         )
+
     model.add(layers.Flatten())
     model.add(layers.Dense(units=hp.Int("units", 128, 512, step=32), activation="relu"))
     model.add(layers.Dense(7, activation="softmax"))
@@ -56,24 +59,21 @@ def build_model(hp):
     return model
 
 
-tuner = Hyperband(
-    build_model,
-    max_epochs=num_epocs,
-    hyperband_iterations=2,
-    objective="val_accuracy",
-    directory="Fer2013_Recognition",
-    project_name="Fer2013_Recognition",
-)
 tuner = RandomSearch(
     build_model,
-    max_trials=40,
+    max_trials=20,
     objective="val_accuracy",
-    directory="Fer2013_Recognition",
     project_name="Fer2013_Recognition",
+    directory="models/Face_Recog/Fer2013_Recognition",
 )
-tuner.search(X, y, epochs=num_epocs, validation_split=num_val_split)
+
+tuner.search(
+    x=X, y=y, batch_size=8, verbose=1, epochs=num_epocs, validation_split=num_val_split
+)
 
 best_hp = tuner.get_best_hyperparameters(1)[0]
 best_model = build_model(best_hp)
-best_model.fit(X, y, epochs=num_epocs, validation_split=num_val_split)
-best_model.save("Fer2013_Recognition.h5")
+
+best_model.fit(X, y, epochs=num_epocs, validation_split=0.2)
+
+best_model.save("face_emotion_model.h5")
