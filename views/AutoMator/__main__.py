c7375ad12bffa4fc94e6cd934bb642bd1d5ceff5
Mågnëum / <krakinzkon@gmail.com>
2023-04-14 10:58:59 +0530

    Miles Voice Assistant

--- views/AutoMator/__main__.py
+++ views/AutoMator/__main__.py
@@ -7,7 +7,7 @@ from colorama import Fore, Style
 
 load_dotenv()
 
-# ============================================================ [ CREATED BY MAGNEUM ]
+# ======================================================================================================================== [ CREATED BY MAGNEUM ]
 # Get environment variables
 INITIAL_TASK = os.getenv("INITIAL_TASK", os.getenv("FIRST_TASK", ""))
 OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-3.5-turbo")
@@ -27,17 +27,17 @@ else:
     print(f"{Fore.GREEN}Environment variables are present.{Style.RESET_ALL}")
 
 
-# ============================================================ [ CREATED BY MAGNEUM ]
+# ======================================================================================================================== [ CREATED BY MAGNEUM ]
 if "gpt-4" in OPENAI_MODEL.lower():
     print(
         f"{Fore.RED}{Style.BRIGHT}"
-        + "\n======[ USING GPT-4. POTENTIALLY EXPENSIVE. MONITOR YOUR COSTS ]======\n"
+        + "\n============[ USING GPT-4. POTENTIALLY EXPENSIVE. MONITOR YOUR COSTS ]============\n"
         + f"{Style.RESET_ALL}"
     )
 
 print(
     f"{Fore.BLUE}{Style.BRIGHT}"
-    + "\n======[ OBJECTIVE ]======\n"
+    + "\n============[ OBJECTIVE ]============\n"
     + f"{Style.RESET_ALL}"
 )
 print(f"{OBJECTIVE}")
@@ -48,7 +48,7 @@ print(
     + f" {INITIAL_TASK}"
 )
 
-# ============================================================ [ CREATED BY MAGNEUM ]
+# ======================================================================================================================== [ CREATED BY MAGNEUM ]
 openai.api_key = OPENAI_API
 pinecone.init(api_key=PINECONE_API, environment=PINECONE_ENV)
 Table_Name = TABLE_NAME.lower()
@@ -67,21 +67,19 @@ index = pinecone.Index(Table_Name)
 Task_List = deque([])
 
 
-# ============================================================ [ CREATED BY MAGNEUM ]
+# ======================================================================================================================== [ CREATED BY MAGNEUM ]
 def Add_Task(task: Dict):
     Task_List.append(task)
-    print(f"{Fore.GREEN}Task added successfully{Style.RESET_ALL}")
 
 
 def Ada_Embedding(text):
     text = text.replace("\n", " ")
     response = openai.Embedding.create(input=[text], model="text-embedding-ada-002")
     embedding = response["data"][0]["embedding"]
-    print(f"{Fore.CYAN}Text embedded successfully{Style.RESET_ALL}")
     return embedding
 
 
-# ============================================================ [ CREATED BY MAGNEUM ]
+# ======================================================================================================================== [ CREATED BY MAGNEUM ]
 def Openai_Response(
     prompt: str,
     model: str = "gpt-3.5-turbo",
@@ -132,7 +130,7 @@ def Openai_Response(
             return None
 
 
-# ============================================================ [ CREATED BY MAGNEUM ]
+# ======================================================================================================================== [ CREATED BY MAGNEUM ]
 def Agent_TaskCreate(
     objective: str, result: Dict, task_description: str, Task_List: List[str]
 ):
@@ -157,14 +155,10 @@ def Agent_TaskCreate(
         {"task_name": task_name, "task_description": task_description}
         for task_name, task_description in zip(task_names, task_descriptions)
     ]
-    print(f"{Fore.GREEN}Generated New Tasks:{Style.RESET_ALL}")
-    for i, task in enumerate(new_tasks, 1):
-        print(f"{Fore.YELLOW}{i}. Task Name: {task['task_name']}")
-        print(f"Task Description: {task['task_description']}{Style.RESET_ALL}")
     return new_tasks
 
 
-# ============================================================ [ CREATED BY MAGNEUM ]
+# ======================================================================================================================== [ CREATED BY MAGNEUM ]
 def Prioritization_Agent(this_task_id: int):
     global Task_List
     task_names = [t["task_name"] for t in Task_List]
@@ -185,16 +179,18 @@ def Prioritization_Agent(this_task_id: int):
             task_id = task_parts[0].strip()
             task_name = task_parts[1].strip()
             Task_List.append({"task_id": task_id, "task_name": task_name})
-            print(
-                f"{Fore.CYAN}{Style.BRIGHT}[INFO] Task {i}: {task_name}{Style.RESET_ALL}"
-            )
+            print(Fore.CYAN + Style.BRIGHT + "[INFO] Task {}: {}".format(i, task_name))
+            print(Style.RESET_ALL)
         else:
             print(
-                f"{Fore.RED}{Style.BRIGHT}[ERROR] Invalid task format: {task_string}{Style.RESET_ALL}"
+                Fore.RED
+                + Style.BRIGHT
+                + "[ERROR] Invalid task format: {}".format(task_string)
             )
+            print(Style.RESET_ALL)
 
 
-# ============================================================ [ CREATED BY MAGNEUM ]
+# ======================================================================================================================== [ CREATED BY MAGNEUM ]
 def Context_Agent(query: str, n: int):
     query_embedding = Ada_Embedding(query)
     results = index.query(
@@ -211,53 +207,115 @@ def Execution_Agent(objective: str, task: str) -> str:
     Take into account these previously completed tasks: {context}\n.
     Your task: {task}\nResponse:"""
     response = Openai_Response(prompt, temperature=0.7, max_tokens=2000)
-    print(f"{Fore.MAGENTA}Execution Plan:{Style.RESET_ALL} {response}")
+    print(Fore.MAGENTA + "Execution Plan:" + Style.RESET_ALL + " " + response)
     return response
 
 
-# ============================================================ [ CREATED BY MAGNEUM ]
-# Initial task
-First_Task = {"task_id": 1, "task_name": "INITIAL_TASK"}
+# ======================================================================================================================== [ CREATED BY MAGNEUM ]
+First_Task = {"task_id": 1, "task_name": INITIAL_TASK}
 Add_Task(First_Task)
 task_id_counter = 1
 
+
+if not os.path.exists("views/AutoMator"):
+    os.makedirs("views/AutoMator")
+
 while True:
     if Task_List:
-        print(
-            f"{Fore.MAGENTA}{Style.BRIGHT}\n======[ TASK LIST ]======\n{Style.RESET_ALL}"
-        )
+        print(Fore.MAGENTA + Style.BRIGHT + "\n============[ TASK LIST ]============\n")
+        print(Style.RESET_ALL)
         for t in Task_List:
-            print(f"{t['task_id']}: {t['task_name']}")
+            print(str(t["task_id"]) + ": " + t["task_name"])
         task = Task_List.popleft()
-        print(
-            f"{Fore.GREEN}{Style.BRIGHT}\n======[ NEXT TASK ]======\n{Style.RESET_ALL}"
-        )
-        print(f"{task['task_id']}: {task['task_name']}")
+        print(Fore.GREEN + Style.BRIGHT + "\n============[ NEXT TASK ]============\n")
+        print(Style.RESET_ALL)
+        print(str(task["task_id"]) + ": " + task["task_name"])
         result = Execution_Agent(OBJECTIVE, task["task_name"])
         this_task_id = int(task["task_id"])
         print(
-            f"{Fore.YELLOW}{Style.BRIGHT}\n======[ TASK RESULT ]======\n{Style.RESET_ALL}"
+            Fore.YELLOW + Style.BRIGHT + "\n============[ TASK RESULT ]============\n"
         )
+        print(Style.RESET_ALL)
         print(result)
         enriched_result = {"data": result}
-        result_id = f"result_{task['task_id']}"
+        result_id = "result_" + str(task["task_id"])
         vector = Ada_Embedding(enriched_result["data"])
         print(
-            f"{Fore.CYAN}{Style.BRIGHT}\n======[ UPDATING INDEX ]======\n{Style.RESET_ALL}"
+            Fore.CYAN + Style.BRIGHT + "\n============[ UPDATING INDEX ]============\n"
         )
-        print(f"Updating index with result_id: {result_id}")
-        # print(f"vector: {vector}")
+        print(Style.RESET_ALL)
+        print("Updating index with result_id: " + result_id)
         new_tasks = Agent_TaskCreate(
             OBJECTIVE,
             enriched_result,
             task["task_name"],
             [t["task_name"] for t in Task_List],
         )
-
         for new_task in new_tasks:
             task_id_counter += 1
             new_task.update({"task_id": task_id_counter})
             Add_Task(new_task)
         Prioritization_Agent(this_task_id)
-
+        output_list = []
+        output_list.append({"title": "TASK LIST", "data": []})
+        for t in Task_List:
+            output_list[-1]["data"].append(str(t["task_id"]) + ": " + t["task_name"])
+        output_list.append(
+            {
+                "title": "NEXT TASK",
+                "data": str(task["task_id"]) + ": " + task["task_name"],
+            }
+        )
+        output_list.append({"title": "TASK RESULT", "data": result})
+        output_list.append(
+            {
+                "title": "UPDATING INDEX",
+                "data": "Updating index with result_id: " + result_id,
+            }
+        )
+        file_name = f"views/AutoMator/output_{result_id}.json"
+        with open(file_name, "w") as f:
+            json.dump(output_list, f)
+            print(
+                Fore.GREEN
+                + Style.BRIGHT
+                + "File updated: "
+                + os.path.abspath(f.name)
+                + Style.RESET_ALL
+            )
     time.sleep(1)
+
+
+# while True:
+#     if Task_List:
+#         print(Fore.MAGENTA + Style.BRIGHT + "\n============[ TASK LIST ]============\n")
+#         for t in Task_List:
+#             print(t["task_id"] + ": " + t["task_name"])
+#         task = Task_List.popleft()
+#         print(Fore.GREEN + Style.BRIGHT + "\n============[ NEXT TASK ]============\n")
+#         print(Style.RESET_ALL)
+#         print(task["task_id"] + ": " + task["task_name"])
+#         result = Execution_Agent(OBJECTIVE, task["task_name"])
+#         this_task_id = int(task["task_id"])
+#         print(Fore.YELLOW + Style.BRIGHT + "\n============[ TASK RESULT ]============\n")
+#         print(Style.RESET_ALL)
+#         print(result)
+#         enriched_result = {"data": result}
+#         result_id = "result_" + task["task_id"]
+#         vector = Ada_Embedding(enriched_result["data"])
+#         print(Fore.CYAN + Style.BRIGHT + "\n============[ UPDATING INDEX ]============\n")
+#         print(Style.RESET_ALL)
+#         print("Updating index with result_id: " + result_id)
+#         new_tasks = Agent_TaskCreate(
+#             OBJECTIVE,
+#             enriched_result,
+#             task["task_name"],
+#             [t["task_name"] for t in Task_List],
+#         )
+#         for new_task in new_tasks:
+#             task_id_counter += 1
+#             new_task.update({"task_id": task_id_counter})
+#             Add_Task(new_task)
+#         Prioritization_Agent(this_task_id)
+
+#     time.sleep(1)
