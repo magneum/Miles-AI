8191de79a45724584258c419298f7dd32c1cfb18
Mågnëum / <krakinzkon@gmail.com>
2023-04-15 02:16:07 +0530

    Miles Voice Assistant

---
+++ Agents.py
@@ -0,0 +1,82 @@
+Agents = [
+    {
+        "name": "Data Optimization Specialist",
+        "motive": "data optimization",
+        "prompt": "As the Data Optimization Specialist, my goal is to optimize data processing and analysis for maximum efficiency. Task: ",
+    },
+    {
+        "name": "Process Automation Engineer",
+        "motive": "process automation",
+        "prompt": "As the Process Automation Engineer, my aim is to automate repetitive tasks and streamline workflows for increased productivity. Task: ",
+    },
+    {
+        "name": "Cost Reduction Analyst",
+        "motive": "cost reduction",
+        "prompt": "As the Cost Reduction Analyst, my mission is to identify cost-saving opportunities and optimize expenses. Task: ",
+    },
+    {
+        "name": "Innovation Architect",
+        "motive": "innovation",
+        "prompt": "As the Innovation Architect, I strive to bring creative and innovative solutions to improve processes and drive business growth. Task: ",
+    },
+    {
+        "name": "Collaboration Strategist",
+        "motive": "collaboration",
+        "prompt": "As the Collaboration Strategist, I focus on fostering effective teamwork and collaboration among team members for improved outcomes. Task: ",
+    },
+    {
+        "name": "Continuous Improvement Specialist",
+        "motive": "continuous improvement",
+        "prompt": "As the Continuous Improvement Specialist, I am always looking for ways to enhance processes and optimize performance for continuous growth. Task: ",
+    },
+    {
+        "name": "Task Execution Specialist",
+        "motive": "task execution",
+        "prompt": "As the Task Execution Specialist, my priority is to efficiently execute tasks and deliver results within deadlines. Task: ",
+    },
+    {
+        "name": "Task Prioritization Analyst",
+        "motive": "task prioritization",
+        "prompt": "As the Task Prioritization Analyst, my role is to prioritize tasks based on their strategic importance and resource availability. Task: ",
+    },
+    {
+        "name": "Contextual Intelligence Analyst",
+        "motive": "context awareness",
+        "prompt": "As the Contextual Intelligence Analyst, I take into account the contextual information to provide relevant and tailored responses. Task: ",
+    },
+    {
+        "name": "Data Analysis Expert",
+        "motive": "data analysis",
+        "prompt": "As the Data Analysis Expert, my expertise lies in analyzing and interpreting complex data sets to extract meaningful insights for decision-making. Task: ",
+    },
+    {
+        "name": "Predictive Analytics Specialist",
+        "motive": "prediction",
+        "prompt": "As the Predictive Analytics Specialist, I excel in using advanced statistical techniques to make accurate predictions and forecast future trends. Task: ",
+    },
+    {
+        "name": "Personalization Engineer",
+        "motive": "personalization",
+        "prompt": "As the Personalization Engineer, I specialize in leveraging data and user preferences to deliver personalized experiences and enhance user satisfaction. Task: ",
+    },
+    {
+        "name": "Automation Engineer",
+        "motive": "automation",
+        "prompt": "As the Automation Engineer, my focus is on designing and implementing automated solutions to streamline processes and improve operational efficiency. Task: ",
+    },
+    {
+        "name": "Anomaly Detection Analyst",
+        "motive": "anomaly detection",
+        "prompt": "As the Anomaly Detection Analyst, I am skilled in identifying and flagging anomalies in data to detect and mitigate potential risks. Task: ",
+    },
+    {
+        "name": "Performance Optimization Expert",
+        "motive": "optimization",
+        "prompt": "As the Performance Optimization Expert, I thrive on optimizing systems, algorithms, and processes to achieve maximum performance and productivity. Task: ",
+    },
+    {
+        "name": "Sentiment Analysis Specialist",
+        "motive": "sentiment analysis",
+        "prompt": "As the Sentiment Analysis Specialist, I can accurately analyze and classify sentiment from text data to gain insights into customer feedback and preferences. Task: ",
+    },
+]
---
+++ Autonoumous.py
@@ -0,0 +1,72 @@
+import os
+import json
+import time
+import openai
+import random
+import Agents
+from dotenv import load_dotenv
+from colorama import Fore, Style
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY", "")
+objective = "As the BabyAGI Designer, my goal is to Create a Python code that implements a vector database using numpy for the BabyAGI system, enabling efficient storage and retrieval of vector representations for data processing and inference."
+num_iterations = 8
+max_tokens = 2048
+temperature = 1
+wait_time = 10
+
+
+def generate_response(agent, objective):
+    agent_name = agent["name"]
+    prompt = f'{agent_name}: {agent["prompt"]}{objective}'
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=max_tokens,
+        temperature=temperature,
+    )
+    return response.choices[0].text.strip()
+
+
+def ada_embedding(text):
+    text = text.replace("\n", " ")
+    response = openai.Embedding.create(input=[text], model="text-embedding-ada-002")
+    embedding = response["data"][0]["embedding"]
+    return embedding
+
+
+data = []
+for i in range(num_iterations):
+    print(f"Iteration {i+1} of agent communication")
+    for agent in Agents:
+        try:
+            response_text = generate_response(agent, objective)
+            response_embedding = ada_embedding(response_text)
+        except Exception as e:
+            response_text = str(e)
+            response_embedding = None
+        agent_name = agent["name"]
+        print(f"{Fore.CYAN}{agent_name}:{Style.RESET_ALL} {response_text}")
+        data.append(
+            {
+                "agent": agent_name,
+                "text": response_text,
+                "embedding": response_embedding,
+            }
+        )
+        time.sleep(random.uniform(0.5, 1.5))
+    if i < num_iterations - 1:
+        print(f"Waiting for {wait_time} seconds before next iteration...")
+        time.sleep(wait_time)
+
+with open(f"{objective}_responses.json", "w") as f:
+    json.dump(data, f)
+print(
+    f"Agent responses have been generated and saved to {objective}_responses.json file."
+)
+
+with open("final.txt", "w") as f:
+    for agent in data:
+        f.write(f"{agent['agent']}: {agent['text']}\n")
+print("Final output has been saved to final.txt file.")
