54cad0cd16e24b513110373d0148444b96bc1115
Mågnëum / <krakinzkon@gmail.com>
2023-04-15 11:53:56 +0530

    Miles AI Voice Assistant

--- EmoTest.py
+++ EmoTest.py
@@ -1,26 +1,19 @@
 import os
 import cv2
+import keras
 import numpy as np
 import urllib.request
 from colorama import Fore, Style
-from keras.models import load_model
 
 cascade_file_path = "corpdata/Fer2013-img/haarcascade_frontalface_default.xml"
 if not os.path.exists(cascade_file_path):
     cv2_url = "https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml"
-    print(
-        Fore.YELLOW
-        + "Haar cascade classifier file not found. Downloading..."
-        + Style.RESET_ALL
-    )
+    print(Fore.YELLOW + "Haar cascade classifier file not found. Downloading...")
+    print(Style.RESET_ALL)
     urllib.request.urlretrieve(cv2_url, cascade_file_path)
-    print(
-        Fore.GREEN
-        + "Haar cascade classifier file downloaded successfully!"
-        + Style.RESET_ALL
-    )
-
-emotion_model = load_model("face_emotion_model.h5")
+    print(Fore.GREEN + "Haar cascade classifier file downloaded successfully!")
+    print(Style.RESET_ALL)
+emotion_model = keras.models.load_model("face_emotion_model.h5")
 face_cascade = cv2.CascadeClassifier(cascade_file_path)
 
 
@@ -30,6 +23,7 @@ def main():
         ret, frame = Capture.read()
         if not ret:
             break
+        print("Captured frame")
         Grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         Faces = face_cascade.detectMultiScale(
             Grayscale, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30)
@@ -49,8 +43,10 @@ def main():
                 "Surprise",
                 "Neutral",
             ]
+            predicted_emotion = emotion_labels[np.argmax(emotions)]
+            print("Predicted emotion from captured face: ", predicted_emotion)
+
         username = None
-        predicted_emotion = emotion_labels[np.argmax(emotions)]
         for File in os.listdir("userNamedfaces"):
             img = cv2.imread(os.path.join("userNamedfaces", File), cv2.IMREAD_GRAYSCALE)
             img = cv2.resize(img, (48, 48))
@@ -58,24 +54,17 @@ def main():
             img = np.expand_dims(img, axis=0)
             img_emotions = emotion_model.predict(img)
             img_predicted_emotion = emotion_labels[np.argmax(img_emotions)]
+            print("Predicted emotion from stored face: ", img_predicted_emotion)
             if img_predicted_emotion == predicted_emotion:
                 username = File.split(".")[0]
                 break
         if username:
+            print(Fore.CYAN + "Detected username: " + Fore.YELLOW + f"{username}")
+            print(Style.RESET_ALL)
             print(
-                Fore.CYAN
-                + "Detected username: "
-                + Fore.YELLOW
-                + f"{username}"
-                + Style.RESET_ALL
+                Fore.CYAN + "Predicted emotion: " + Fore.YELLOW + f"{predicted_emotion}"
             )
-            print(
-                Fore.CYAN
-                + "Predicted emotion: "
-                + Fore.YELLOW
-                + f"{predicted_emotion}"
-                + Style.RESET_ALL
-            )
+            print(Style.RESET_ALL)
         cv2.imshow("Emotion Detection", frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
