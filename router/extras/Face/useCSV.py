21e196344a3896aa9f31e0d06db7260924441f56
Mågnëum / <krakinzkon@gmail.com>
2023-04-15 22:31:51 +0530

    Miles AI Voice Assistant

--- router/extras/Face/useCSV.py
+++ router/extras/Face/useCSV.py
@@ -9,18 +9,7 @@ from keras.callbacks import EarlyStopping
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 
 
-# print(f"{Fore.YELLOW}{Style.BRIGHT}Code Description: FaceProcessing.py")
-# print(f"{Fore.WHITE}{Style.BRIGHT}------------------")
-# print(f"{Fore.CYAN}{Style.BRIGHT}The code is a Python script that uses Keras and TensorFlow libraries to perform hyperparameter tuning for a convolutional neural network (CNN) model for facial emotion recognition.")
-# print(f"{Fore.CYAN}{Style.BRIGHT}The dataset used is Fer2013, which is loaded from a CSV file.")
-# print(f"\n{Fore.CYAN}{Style.BRIGHT}The script uses the keras_tuner library to perform random search hyperparameter tuning. The Hyper_Builder() function defines the architecture of the CNN model and compiles it with hyperparameters such as filters, kernel size, pool size, units, and learning rate. The hyperparameters are sampled from defined ranges using the hp object, which is passed as an argument to the function.")
-# print(f"\n{Fore.CYAN}{Style.BRIGHT}The Early_Stopping() function defines early stopping as a callback for the model during training, which prevents overfitting. The patience for early stopping is also hyperparameterized using the hp object.")
-# print(f"\n{Fore.CYAN}{Style.BRIGHT}The Hyperband tuner is then created with the Hyper_Builder() function as the model-building function, and the maximum number of trials, project name, and objective for tuning (in this case, 'val_accuracy') are specified. The tuner searches for the best hyperparameters using the search() function, which takes the input data (X_Index and Y_Index), number of epochs, batch size, and other parameters.")
-# print(f"\n{Fore.CYAN}{Style.BRIGHT}After tuning is completed, the best hyperparameters and their corresponding model performance metrics (such as accuracy, loss) are printed. The best model is then trained with the optimal hyperparameters and evaluated on the validation set. Finally, the model is saved to disk for future use.")
-# print(f"{Style.RESET_ALL}")
-
-
-# Hyper Variables
+# ========================================================= Magneum =========================================================
 X_Index = []
 Y_Index = []
 nSeed = 22
@@ -34,12 +23,15 @@ dataset_path = "corpdata/csv/fer2013/fer2013.csv"
 model_save_path = "models/Face_Emo/Face_Emo_Model.h5"
 
 
+# ========================================================= Magneum =========================================================
 file_path = os.path.abspath(__file__)
 file_name = os.path.basename(file_path)
 _path = "models/FaceEmo"
 if not os.path.exists(_path):
     os.makedirs(_path)
 
+
+# ========================================================= Magneum =========================================================
 Fer2013 = pandas.read_csv(dataset_path)
 for index, row in Fer2013.iterrows():
     pixels = numpy.fromstring(row["pixels"], dtype="uint8", sep=" ")
@@ -51,6 +43,7 @@ X_Index = numpy.array(X_Index)
 Y_Index = numpy.array(Y_Index)
 
 
+# ========================================================= Magneum =========================================================
 def Hyper_Builder(hp):
     model = Sequential()
     model.add(
@@ -90,6 +83,9 @@ def Hyper_Builder(hp):
     return model
 
 
+# ========================================================= Magneum =========================================================
+
+
 Hyper_Tuner = Hyperband(
     Hyper_Builder,
     seed=nSeed,
@@ -98,8 +94,6 @@ Hyper_Tuner = Hyperband(
     objective="val_accuracy",
     directory=hyper_directory,
 )
-
-
 Hyper_Tuner.search(
     x=X_Index,
     y=Y_Index,
@@ -112,6 +106,8 @@ Hyper_Tuner.search(
     ],
 )
 
+
+# ========================================================= Magneum =========================================================
 BestHP = Hyper_Tuner.get_best_hyperparameters(1)[0]
 Hyper_Model = Hyper_Builder(BestHP)
 Hyper_Model.fit(X_Index, Y_Index, epochs=nEpochs, validation_split=0.2)
